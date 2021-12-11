f2533d8eafdd0558689fa517a242ebac64925ad9
chenkai036 <chenkai036@users.noreply.github.com>
2021-12-11 19:42:16 +0100

    day3 - 2nd half

--- day3/solution.py
+++ day3/solution.py
@@ -14,7 +14,32 @@ def solution1(data: pandas.Series) -> int:
     return gamma * epsilon
 
 
+def solution2(data: pandas.Series) -> int:
+    def find_rating(criteria_fn):
+        current, bp = series, 0
+        while len(current) != 1:
+            bits = (current & masks[bp]) != 0
+            current = current[bits == criteria_fn(len(bits), sum(bits))]
+            bp += 1
+        return int(current)
+
+    def mcv(nsize, ones):
+        return not (ones * 2 < nsize)
+
+    def lcv(nsize, ones):
+        return ones * 2 < nsize
+
+    series = data.apply(lambda v: int(v, base=2))
+    nbits = data.apply(len).max()
+    masks = [1 << (nbits - i - 1) for i in range(nbits)]
+    oxygen = find_rating(mcv)
+    co2 = find_rating(lcv)
+
+    return oxygen * co2
+
+
 if __name__ == '__main__':
     series = pandas.read_csv('input.txt', header=None, squeeze=True, dtype=str)
     print(solution1(series))
+    print(solution2(series))
 
