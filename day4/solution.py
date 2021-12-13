69ecd7883fbc5fa2991d1eaf27906f7e3416c2c5
chenkai036 <chenkai036@users.noreply.github.com>
2021-12-13 23:12:06 +0100

    day4 full

--- day4/solution.py
+++ day4/solution.py
@@ -13,38 +13,32 @@ def read_input(filepath):
     return numbers, boards
 
 
-def solution2(data):
-    draws, boards = data
-    dframe = pandas.concat(boards + [d.T for d in boards], ignore_index=True)
-    index = pandas.DataFrame(False, index=dframe.index, columns=dframe.columns)
-    for number in draws:
-        index = index | (dframe == number)
-        aggregated = index.all(axis=1)
-        if numpy.any(aggregated):
-            row = aggregated[aggregated == True].index
-            board_id = (row.values[0] % len(boards)) // N
-            marked_index = index.loc[board_id * N: board_id * N + 4, :].reset_index(drop=True)
-            unmarked = boards[board_id] * ~marked_index
-            return number * unmarked.values.sum()
+def win_when(current_index, prev_index, n):
+    if len(current_index) == n and len(prev_index) == n - 1:
+        return int(current_index.difference(prev_index).values)
+    return None
 
 
-def solution1(data):
-    draws, boards = data
+def solution(draws, boards, win_value):
     dframe = pandas.concat(boards + [d.T for d in boards], ignore_index=True)
-    index = pandas.DataFrame(False, index=dframe.index, columns=dframe.columns)
-    for number in draws:
-        index = index | (dframe == number)
-        aggregated = index.all(axis=1)
-        if numpy.any(aggregated):
-            row = aggregated[aggregated == True].index
-            board_id = (row.values[0] % len(boards)) // N
-            marked_index = index.loc[board_id * N: board_id * N + 4, :].reset_index(drop=True)
-            unmarked = boards[board_id] * ~marked_index
-            return number * unmarked.values.sum()
+    prev_index = pandas.Int64Index([])
+    for i, n in enumerate(draws):
+        numbers = draws[:i + 1]
+        matched_rc = dframe.isin(numbers).all(axis=1)
+        matched_board = matched_rc.groupby(matched_rc.index // N).any()
+        matched_board = matched_board.groupby(matched_board.index % len(boards)).any()
+        current_index = matched_board[matched_board].index
+        if len(current_index) == win_value and len(prev_index) == win_value - 1:
+            board = boards[int(current_index.difference(prev_index).values)]
+            unmarked_board = board * ~board.isin(numbers)
+            return n * unmarked_board.values.sum()
+        else:
+            prev_index = current_index
 
 
 if __name__ == '__main__':
-    data = read_input('input.txt')
-    print(solution1(data))
-    print(solution2(data))
+    draws, boards = read_input('input.txt')
+    print(solution(draws, boards, 1))
+    print(solution(draws, boards, len(boards)))
+
 
